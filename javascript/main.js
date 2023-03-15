b7752eb23977e6efe46896adee3d4e98daa6597d
lngdao <dtlong.2101@gmail.com>
2023-03-15 22:54:31 +0700

    feat(*): up

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,8 @@
-const path = require("path");
+const path = require('path');
 
 // variable
 var a = 1;
-var b = "abc";
+var b = 'abc';
 var c = 1.5;
 let d = [1, 2, 3, 4];
 const e = Math.PI;
@@ -10,9 +10,9 @@ const e = Math.PI;
 console.log(a, b, c, d, e);
 
 // noi chuoi
-let string1 = "dao";
-let string2 = "long";
-let string3 = string1 + " " + string2;
+let string1 = 'dao';
+let string2 = 'long';
+let string3 = string1 + ' ' + string2;
 
 console.log(string3);
 console.log(`${string1} ${string2}`); // literal string
@@ -52,7 +52,7 @@ function showInformation(name, age, address) {
   console.log(`name: ${name}\nage: ${age}\naddress: ${address}`);
 }
 
-showInformation("long dao", 22, "Ha noi");
+showInformation('long dao', 22, 'Ha noi');
 
 // bt2: viet ham tinh dien tich tam giac va hien thi ra man hinh canh1: , canh2: , dien tich:
 
@@ -122,3 +122,11 @@ const newArray2 = aArray.fillter2((item) => typeof item == 'number');
 console.log(newArray2);
 
 // form validation
+
+// promise
+const aPromise = new Promise((resolve, reject) => {
+  setTimeout(() => resolve('response'), 1000);
+});
+
+// side effect: call api, ...
+aPromise.then((res) => console.log(res)).catch((err) => console.log(err)); 
