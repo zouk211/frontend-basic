5d44467ce6e27d4df7d810e09e6731a0cafe6c6d
lngdao <dtlong.2101@gmail.com>
2023-03-15 22:02:46 +0700

    feat(*): demo fetch

---
+++ basic/promise/index.js
@@ -0,0 +1,30 @@
+const colorsWrapper = document.querySelector('.colors');
+
+const loadingEle = document.createElement('span');
+loadingEle.textContent = 'Loading data...';
+
+colorsWrapper.appendChild(loadingEle);
+
+const createColorElement = (hex) => {
+  const colorEle = document.createElement('div');
+  colorEle.textContent = hex;
+  colorEle.classList.add('color');
+  colorEle.style.background = hex;
+
+  colorEle.addEventListener('click', () => alert(hex));
+
+  return colorEle;
+};
+
+// fetch API
+setTimeout(() => {
+  fetch('https://reqres.in/api/unknown')
+    .then((res) => res.json())
+    .then((data) => {
+      colorsWrapper.removeChild(loadingEle);
+      data.data.forEach((item) => {
+        const hex = item.color;
+        colorsWrapper.appendChild(createColorElement(hex));
+      });
+    });
+}, 3000);
